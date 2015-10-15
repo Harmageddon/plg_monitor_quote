d277046cdecbcaf3a401fd57bb6d7a3f636af0a0
Constantin Romankiewicz <constantin@zweiiconkram.de>
2015-10-15 18:12:46 +0200

    Added javascript for quote button.

--- monitorquote.php
+++ monitorquote.php
@@ -58,13 +58,20 @@ class PlgContentMonitorQuote extends JPlugin
 		}
 
 		$regex = "/\{quote\=(.*?)\,(\d*)\.(\d*)\}(.*?)\{\/quote\}/";
-		$source = '';
-		$replacement = '<blockquote>$3</blockquote>';
 
 		$item->text = preg_replace_callback($regex, 'PlgContentMonitorQuote::replace', $item->text);
-
 	}
 
+	/**
+	 * Generates the quote HTML code for the regex used in onContentPrepare.
+	 *
+	 * @param   array  $match  Array holding the matches inside the text.
+	 *                         array(fullMatch, username, issueId, commentId, content)
+	 *
+	 * @return string
+	 *
+	 * @see onContentPrepare
+	 */
 	public static function replace($match)
 	{
 		$url = JRoute::_('index.php?option=com_monitor&view=issue&id=' . (int) $match[2] . '#comment-' . (int) $match[3]);
@@ -75,5 +82,53 @@ class PlgContentMonitorQuote extends JPlugin
 		);
 
 		return JLayoutHelper::render('quote', $displayData, __DIR__ . '/layouts');
+	}
+
+	/**
+	 * Adds a javascript file at the bottom of the page.
+	 *
+	 * @param   string   $context  The context of the content being passed to the plugin.
+	 * @param   mixed    &$item    The item displayed.
+	 * @param   mixed    $params   Additional parameters.
+	 * @param   integer  $page     Optional page number.
+	 *
+	 * @return  string|null  HTML code to include the script, if the plugin is loaded on a single issue page.
+	 */
+	public function onContentAfterDisplay($context, &$item, $params, $page = 0)
+	{
+		// Check if the component is installed and enabled.
+		if (!JComponentHelper::isEnabled('com_monitor'))
+		{
+			return null;
+		}
+
+		$allowed_contexts = array('com_monitor.issue');
+
+		if (!in_array($context, $allowed_contexts))
+		{
+			return null;
+		}
+
+		$script = JHtml::_('script', 'plg_content_monitorquote/monitor_quote.js', false, true, true);
+
+		if (!$script)
+		{
+			return null;
+		}
+
+		// Include language strings for javascript.
+		$langStrings = array(
+			'PLG_CONTENT_MONITORQUOTE_QUOTE_THIS',
+		);
+
+		foreach ($langStrings as $langString)
+		{
+			JText::script($langString);
+		}
+
+		return '<script
+			type="text/javascript"
+		    src="' . $script . '"
+		    ></script>';
 	}
 }
